70799f66bea5b20998b398ce9cfdaa8d76258199
Ryan Farquharson <ryan.farquharson@csiro.au>
2018-08-09 21:02:31 +0930

    Did some more exploration.  Added month and year to eUGE tables so that point plots of electricity for each month across all years could be plotted.

--- scripts/script1.R
+++ scripts/script1.R
@@ -161,6 +161,79 @@ mean(perday$totalusage_perday, na.rm = TRUE)
 eUGE <-  
   select(perday,"date","peak_perday","offpeak_perday","totalusage_perday",
          "PVgen_perday","feedin_perday","PVhours_perday","PVefficiency", "feedinefficiency")
+eUGE1
+
+eUGE1 <- select(eUGE1, -"month")
+
+monthlist <- c(6,7,8,9,10,11,12,1,2,3,4,5)
+monthlist <- c(monthlist, monthlist)
+monthlist <- c(monthlist,6,7,8,9,10,11,12,1,2,3,4,5)
+monthlist
+
+eUGE1 <- slice(eUGE1, -109)
+
+write.csv(eUGE1, file = "eUGE1.csv")
+
+eUGE2 <- eUGE1 %>% 
+  mutate(month = monthlist)
+
+yearlist1 <- c(2009,2009,2009,2009,2009,2009,2009)
+yearlist2 <- c(2010,2010,2010,2010,2010,2010,2010,2010,2010,2010,2010,2010)
+yearlist3 <- yearlist2 + 1
+yearlist4 <- yearlist2 + 2
+yearlist5 <- yearlist2 + 3
+yearlist6 <- yearlist2 + 4
+yearlist7 <- yearlist2 + 5
+yearlist8 <- yearlist2 + 6
+yearlist9 <- yearlist2 + 7
+yearlist10 <- c(2018,2018,2018,2018,2018)
+
+yearlist <- c(yearlist1,yearlist2,yearlist3,yearlist4,yearlist5,yearlist6,yearlist7,yearlist8,yearlist9,yearlist10)
+yearlist
+
+eUGE3 <- eUGE2 %>% 
+  mutate(year = yearlist)
+
+ggplot(eUGE3, aes(x = as.factor(month), y = peak_perday, colour = year)) +
+  geom_point() +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(fill = NA))
+
+ggplot(eUGE3, aes(x = as.factor(month), y = offpeak_perday, colour = year)) +
+  geom_point() +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(fill = NA))
+
+ggplot(eUGE3, aes(x = as.factor(month), y = totalusage_perday, colour = year)) +
+  geom_point() +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(fill = NA))
+
+ggplot(eUGE3, aes(x = as.factor(month), y = PVgen_perday, colour = year)) +
+  geom_point() +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(fill = NA))
+
+ggplot(eUGE3, aes(x = as.factor(month), y = feedin_perday, colour = year)) +
+  geom_point() +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(fill = NA))
+
+ggplot(eUGE3, aes(x = as.factor(month), y = PVhours_perday, colour = year)) +
+  geom_point() +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(fill = NA))
+
+ggplot(eUGE3, aes(x = as.factor(month), y = PVefficiency, colour = year)) +
+  geom_point() +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(fill = NA))
+
+ggplot(eUGE3, aes(x = as.factor(month), y = feedinefficiency, colour = year)) +
+  geom_point() +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.border = element_rect(fill = NA))
+
 
 
 # It's interesting to note that a few things have happened in our household
